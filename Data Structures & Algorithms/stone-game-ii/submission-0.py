11ff08f2c3d090bb74162b55501609984b8a9937
Arnold G. Waigwa <waigwaarnold@gmail.com>
2026-07-22 17:06:43 -0400

    Add: stone-game-ii - submission-0

---
+++ Data Structures & Algorithms/stone-game-ii/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:  
+        """
+        piles = [3, 1, 2, 5, 7]
+        suffix  [18,15,14,12,7,0] suffix[i] = suffix[i + 1] + piles[i]
+        dfs(i, M) => suffix[i]: 18 - dfs(i) ,
+        dfs(4, 1) 7 + dfs(5, )
+        """
+        n = len(piles)
+        suffix = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suffix[i] += suffix[i + 1] + piles[i]
+        
+        dp = {}
+        
+        def dfs(i, M):
+            if (i, M) in dp:
+                return dp[(i, M)]
+            
+            if i + 2 * M >= n:
+                return suffix[i]
+
+            if i >= n:
+                return 0
+            
+            dp[(i, M)] = 0
+            for j in range(i, min(i + 2 * M , n)):
+                dp[(i, M)] = max(
+                    dp[(i, M)],
+                    suffix[i] - dfs(j + 1, max(j - i + 1, M))
+                )
+            
+            return dp[(i, M)]
+        
+        return dfs(0, 1)
+                
+
+
+
