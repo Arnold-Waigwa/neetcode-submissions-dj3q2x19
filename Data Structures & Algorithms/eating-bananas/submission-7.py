4e31fd2f097211fe2c546c649bb6bc18ac1745fa
Arnold G. Waigwa <waigwaarnold@gmail.com>
2026-08-29 11:16:21 -0400

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+
+        def canComplete(rate):
+            time = 0
+
+            for pile in piles:
+                time += (pile + rate - 1) // rate
+
+                if time > h:
+                    return False
+
+            return True
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if canComplete(mid):
+                r = mid
+            else:
+                l = mid + 1
+
+        return r
+
